b7b3e9fffcf0e434bdb2f043242ed0d0a69f4b82
Irina <irishkakhrol@gmail.com>
2023-01-08 12:31:45 -0800

    Changes project MyUniversity

--- MyQAAutoHomeworks/MyUniversity/Program.cs
+++ MyQAAutoHomeworks/MyUniversity/Program.cs
@@ -4,21 +4,21 @@ internal class Program
     static void Main(string[] args)
     {
         UniversityEmployee employee1 = new Teacher (new Person("Ivan", "Ivanov"),0101,
-                                            new Course(" Phyth", "Qvantum Theory"));
+                                            new Course("Phyth", "Qvantum Theory"));
 
         UniversityEmployee employee2 = new DegreeTeacher(new Person("Irina", "Chernyavskaya"), 0201,
-                                            new Course(" Biology", "Brain"),
+                                            new Course("Biology", "Brain"),
                                             "Masterdegree", "Docent");
 
         UniversityEmployee employee3 = new DegreeTeacher(new Person("Olga", "Volnaya"), 0301,
-                                            new Course(" Chemistry", "Air"),
+                                            new Course("Chemistry", "Air"),
                                             "Proffessor", "Docent");
 
         UniversityEmployee employee4 = new SupportStaff(new Person("Igor", "Krylov"), 0401,
                                             " Driver", "Driver Children");
 
         UniversityEmployee employee5 = new SupportStaff(new Person("Peter", "Pen"), 0501,
-                                            " Cleaner", "Clean Window");
+                                            "Cleaner", "Clean Window");
 
         UniversityEmployee[] universityEmployees = new UniversityEmployee[]
         {
